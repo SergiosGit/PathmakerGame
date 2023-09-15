dc413f8120d63f4d756702fe5bb069659f9e79cd
Sergio.Lemaitre <Sergio.Lemaitre@med.ge.com>
2023-09-14 23:45:57 -0500

    added four robot cars controllable by gamepads

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -5,10 +5,6 @@ let imageCenterX  = canvas.width / 2; // Center X coordinate of the image
 let imageCenterY  = canvas.height / 2; // Center Y coordinate of the image
 let carWidth = 100;
 let carHeight = 100;
-let initialCarX = imageCenterX-carWidth/2; // Initial X position
-let initialCarY = imageCenterY-carHeight/2; // Initial Y position
-let squareX = initialCarX; // Initial X position
-let squareY = initialCarY; // Initial Y position
 let ipAddress = 'http://localhost:8082'; // Replace with your server IP
  
 
@@ -24,58 +20,6 @@ let selectedMode = Array.from(gameModeElements).find(radio => radio.checked).val
 console.log(`Initial mode: ${selectedMode}`);
 
 
-// Function to fetch and display high scores
-async function fetchHighScores() {
-    try {
-        const response = await fetch(`${ipAddress}/api/highscores`);
-        const highScores = await response.json();
-        const highScoresList = document.getElementById('highScoresList');
-        highScoresList.innerHTML = ''; // Clear the list
-
-        if (highScores.length === 0) {
-            highScoresList.innerHTML = '<li>No high scores available.</li>';
-        } else {
-            highScores.forEach(score => {
-                const listItem = document.createElement('li');
-                listItem.textContent = `Player: ${score.playerName}, Score: ${score.score}`;
-                highScoresList.appendChild(listItem);
-            });
-        }
-    } catch (error) {
-        console.error('Error fetching high scores:', error);
-    }
-}
-
-
-// Add event listener for form submission to add high score
-document.getElementById('highScoreForm').addEventListener('submit', async function (event) {
-    event.preventDefault();
-    const playerName = document.getElementById('playerName').value;
-    const score = document.getElementById('score').value;
-
-    try {
-        const response = await fetch(`${ipAddress}/api/highscores`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ playerName, score })
-        });
-
-        if (response.status === 200) {
-            console.log('High score submitted successfully.');
-            // Refresh the high scores list
-            fetchHighScores();
-        } else {
-            console.error('Failed to submit high score.');
-        }
-    } catch (error) {
-        console.error('Error submitting high score:', error);
-    }
-});
-
-// Initial fetch of high scores
-fetchHighScores();
 
 
 // Function to send gamepad index and axes values to the backend
@@ -111,7 +55,7 @@ async function sendGamepadAxesToBackend(gamepadIndex, axesValues) {
         })
         .then(data => {
             // Handle the calculated values returned from the server
-            updateSquarePosition(data);
+            updateRobotPosition(data);
             //console.log(data); // The array of calculated values
         })
         .catch(error => {
@@ -122,80 +66,143 @@ async function sendGamepadAxesToBackend(gamepadIndex, axesValues) {
     }
 }
 
-// Check for connected gamepads and send axes values to the backend
 function checkGamepadAxes() {
     const gamepads = navigator.getGamepads();
-    for (let indexGamepad = 0; indexGamepad < gamepads.length; indexGamepad++) {
+    
+    // Check the selected mode
+    if (selectedMode === 'Pathmaker') {  // send data to backend
+        let indexGamepad = 0
+        // Pathmaker mode: Send axesValues to the backend for the selected gamepad
         const gamepad = gamepads[indexGamepad];
         if (gamepad) {
-            const axesValues = gamepad.axes.map(value => value * 1); // Implicit number format conversion Adjust the multiplier as needed
-            // clamp values to zero if they are below a certain threshold
+            const axesValues = gamepad.axes.map(value => value * 1); // Implicit number format conversion. Adjust the multiplier as needed
+            // Clamp values to zero if they are below a certain threshold
             for (let indexAxis = 0; indexAxis < axesValues.length; indexAxis++) {
                 if (Math.abs(axesValues[indexAxis]) < 0.05) {
                     axesValues[indexAxis] = 0;
                 }
             }
-            //console.log("selectedMode: " + selectedMode);
-            sendGamepadAxesToBackend(indexGamepad,axesValues);
-        } 
+            sendGamepadAxesToBackend(indexGamepad, axesValues);
+        }
+    } else {
+        // Multiplayer mode: Update axesValues for all connected gamepads without sending them to the backend
+        var numberOfGamepads = 0;
+        for (let indexGamepad = 0; indexGamepad < gamepads.length; indexGamepad++) {
+            const gamepad = gamepads[indexGamepad];
+            if (gamepad) {
+                numberOfGamepads++;
+                const axesValues = gamepad.axes.map(value => value * 1); // Implicit number format conversion. Adjust the multiplier as needed
+                // Clamp values to zero if they are below a certain threshold
+                for (let indexAxis = 0; indexAxis < axesValues.length; indexAxis++) {
+                    if (Math.abs(axesValues[indexAxis]) < 0.05) {
+                        axesValues[indexAxis] = 0;
+                    } else {
+                        axesValues[indexAxis] = axesValues[indexAxis] * axesValuesScalingFactor[indexAxis];
+                    }
+                }
+                // Update axesValues for the gamepad in the multiplayer mode
+                updateGamepadAxes(indexGamepad, axesValues);
+            }
+        }
+        // Update the robot position based on the actual number of connected gamepads
+        // This ensures that the robot position is only updated if at least one gamepad is connected
+        updateRobotPositions(numberOfGamepads);
+
     }
 }
 
-// Add an event listener to periodically check gamepad axes
-//setInterval(checkGamepadAxes, 1000);
+axesValuesScalingFactor = [];
+axesValuesScalingFactor.length = 4
+axesValuesScalingFactor[0] = 5;
+axesValuesScalingFactor[1] = 5;
+axesValuesScalingFactor[2] = 0.1;
+axesValuesScalingFactor[3] = 1;
+
+// Define an array to store the state of each gamepad
+const gamepadStates = [];
+gamepadStates.length = 4; // Set the length of the array to 4
+gamepadStates[0] = { axes: [100, 400, 0, 0] }; // Initialize the first gamepad state for car_blue_1
+gamepadStates[1] = { axes: [100, 700, 0, 0] }; // Initialize the second gamepad state for car_blue_2    
+gamepadStates[2] = { axes: [1000, 400, 0, 0] }; // Initialize the third gamepad state for car_red_1 
+gamepadStates[3] = { axes: [1000, 700, 0, 0] }; // Initialize the fourth gamepad state for car_red_2
 
 // Function to draw a custom car icon at specified position (x, y)
 // add a parameter for the car image URL provided by the backend
-const carImage = new Image();
-carImage.src = '/images/car.png';
-const gamefieldImage = new Image();
-gamefieldImage.src = '/images/gamefield2023.png';
+//const carImage = document.createElement('img');
+const gamefieldImage = document.createElement('img');
+gamefieldImage.src = "/8_FTC/Software/Pathmaker3/restful/restful/src/main/resources/images/gamefield2023.png";
+gamefieldImage.onload = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+    ctx.drawImage(gamefieldImage, 0, 0, canvas.width, canvas.height);
+};
 
-function drawCarIcon(x, y, rotationAngle) {
+const carImages = [];
+// Function to load an image and push it to the carImages array
+function loadImageAndDraw(src, x, y) {
+    const img = new Image();
+    img.onload = function () {
+        // Once the image is loaded, draw it on the canvas at the specified position (x, y)
+        ctx.drawImage(img, x, y, carWidth, carHeight);
+    };
+    img.src = src;
+    carImages.push(img);
+}
+loadImageAndDraw("/8_FTC/Software/Pathmaker3/restful/restful/src/main/resources/images/car_blue_1.png", gamepadStates[0].axes[0], gamepadStates[0].axes[1]);
+loadImageAndDraw("/8_FTC/Software/Pathmaker3/restful/restful/src/main/resources/images/car_blue_2.png", gamepadStates[1].axes[0], gamepadStates[1].axes[1]);
+loadImageAndDraw("/8_FTC/Software/Pathmaker3/restful/restful/src/main/resources/images/car_red_1.png", gamepadStates[2].axes[0], gamepadStates[2].axes[1]);
+loadImageAndDraw("/8_FTC/Software/Pathmaker3/restful/restful/src/main/resources/images/car_red_2.png", gamepadStates[3].axes[0], gamepadStates[3].axes[1]);
+
+
+
+//if (selectedMode === 'Pathmaker') {
+//    const carImage = new Image();
+//    const gamefieldImage = new Image();
+//    carImage.src = '/images/car.png';
+//    gamefieldImage.src = '/images/gamefield2023.png';
+//}
+
+// Update the gamepad axes in javascript instead of sending them to the backend
+function updateGamepadAxes(indexGamepad, axesValues) {
+    // Check if the indexGamepad is within the valid range (0 to 3)
+    if (indexGamepad >= 0 && indexGamepad < 4) {
+        // Update the axes values for the specified gamepad by performing element-wise addition
+        const updatedAxes = gamepadStates[indexGamepad].axes.map((value, index) => value + axesValues[index]);
+        gamepadStates[indexGamepad].axes = updatedAxes;
+    }
+}
+
+// Function to update the square's position and rotation based on gamepad input
+function updateRobotPositions(numberOfGamepads) {
     // Draw the gamefield image
     ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
     ctx.drawImage(gamefieldImage, 0, 0, canvas.width, canvas.height);
     // Save the current canvas state
-    ctx.save();
-    // move the canvas context to the center of the carImage
-    const dx = x + carWidth/2;
-    const dy = y + carHeight/2;
-    ctx.translate(dx , dy );
-    // Rotate the canvas
-    ctx.rotate(rotationAngle);
-    // Translate back to the original position
-    ctx.translate(-dx , -dy );
-    // Draw the rotated image
-    ctx.drawImage(carImage, x, y, carWidth, carHeight);
-    // Restore the canvas state
-    ctx.restore();
-}
 
-// Function to draw the blue square
-//function drawSquare(x, y) {
-//    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-//    ctx.fillStyle = 'blue'; // Set square color
-//    ctx.fillRect(x, y, squareSize, squareSize); // Draw the square
-//}
-
-// Function to update the square's position and rotation based on gamepad input
-let rotationAngle = 0; // Initial rotation angle
-function updateSquarePosition(axesValues) {
-
-    // Update the square's position based on gamepad input
-    squareX = initialCarX + axesValues[0]; // Adjust X position (left-right movement)
-    squareY = initialCarY + axesValues[1]; // Adjust Y position (up-down movement)
-
-    // Ensure the square stays within the canvas boundaries
-    squareX = Math.max(0, Math.min(canvas.width - squareSize, squareX));
-    squareY = Math.max(0, Math.min(canvas.height - squareSize, squareY));
-
-    // Update the square's rotation based on gamepad input
-    rotationAngle = axesValues[2]; // Adjust rotation angle (left-right movement)
-
-    // Draw the updated square position
-    //drawSquare(squareX, squareY);
-    drawCarIcon(squareX, squareY, rotationAngle);
+    for (let indexGamepad = 0; indexGamepad < numberOfGamepads; indexGamepad++) {
+        ctx.save();
+        // Get the axes values for the specified gamepadStates
+        const carImage = carImages[indexGamepad];
+        const axesValues = gamepadStates[indexGamepad].axes;
+        x = axesValues[0];
+        y = axesValues[1];
+        x = Math.max(0, Math.min(canvas.width - squareSize, x));
+        y = Math.max(0, Math.min(canvas.height - squareSize, y));
+        gamepadStates[indexGamepad].axes[0] = x;
+        gamepadStates[indexGamepad].axes[1] = y;
+        const rotationAngle = axesValues[2];
+        // move the canvas context to the center of the carImage
+        const dx = x + carWidth/2;
+        const dy = y + carHeight/2;
+        ctx.translate(dx , dy );
+        // Rotate the canvas
+        ctx.rotate(rotationAngle);
+        // Translate back to the original position
+        ctx.translate(-dx , -dy );
+        // Draw the rotated image
+        ctx.drawImage(carImage, x, y, carWidth, carHeight);
+        // Restore the canvas state
+        ctx.restore();
+    }
 }
 
 // Add an event listener for gamepad input
@@ -210,29 +217,6 @@ window.addEventListener('gamepadconnected', (event) => {
         checkGamepadAxes();
     }, 20); // Adjust the interval as needed for responsiveness
 });
-
-// Initial draw of the car icon
-carImage.onload = function () {
-    // Send an "initialize" request to the backend
-    fetch('/api/initialize', {
-        method: 'GET',
-    })
-    .then(response => {
-        if (response.status === 200) {
-            // The "initialize" request was successful
-            console.log('Initialization successful');
-            // Now, draw the car icon and handle other logic
-            drawCarIcon(squareX, squareY, rotationAngle);
-        } else {
-            // Handle errors or failed initialization
-            console.error('Initialization failed');
-        }
-    })
-    .catch(error => {
-        console.error('Error during initialization:', error);
-    });
-};
-
 
 // Get a reference to the open charts button element
 const openChartsButton = document.getElementById('openChartsButton');
